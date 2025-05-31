c8f166de4851b2affc885097352b6ce85d590ecd
FredhKhelyan <fredhkamgaing@gmail.com>
2025-05-31 10:01:48 +0100

    Front react initialise

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreStudentRequest;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -105,7 +104,7 @@ class StudentController extends Controller
     /**
      * Inscription d'un étudiant par lui-même.
      */
-    public function enroll(StoreStudentRequest $request)
+    public function enroll(Request $request)
     {
         $user = $request->user();
         if ($user->role !== 'etudiant') {
@@ -121,8 +120,6 @@ class StudentController extends Controller
             'last_name' => 'required|string|max:255',
             'class_id' => 'nullable|exists:classes,id', // Optionnel
         ]);
-
-        // $request->validated();
 
         $lastStudent = Student::orderBy('id', 'desc')->first();
         $nextNumber = $lastStudent ? (int) str_replace('IUT-', '', $lastStudent->matricule) + 1 : 1;
